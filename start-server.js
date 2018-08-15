f924483293962ca6686e4144d639687df627c4fe
Ted <shan81920@gmail.com>
2018-08-15 11:31:43 +0800

    modify. delete__dirname from path

--- start-server.js
+++ start-server.js
@@ -5,10 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/heroku-app-angular'));
+// app.use(express.static(__dirname + '/dist/heroku-app-angular'));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/heroku-app-angular/index.html'));
+// });
+
+app.use(express.static('/dist/heroku-app-angular'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/heroku-app-angular/index.html'));
+    res.sendFile(path.join('/dist/heroku-app-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
