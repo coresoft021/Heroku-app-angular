88749ab94d7ae16109740c5f376be8b36300ad9a
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-08-15 12:35:13 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule,HttpModule,
+    HttpClientModule,HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
