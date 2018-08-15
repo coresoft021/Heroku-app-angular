7d4138d78b098e169e9e6698470308f1825b850a
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-08-15 12:39:16 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,16 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http'; 
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
+
+import { AuthGuard } from './services/auth.guard';
+import { DataService } from './services/data.service';
+import { ReportService } from './services/report.service';
+import { AuthenticationService } from './services/login.service';
+import { UpdateService } from './services/update.service';
+import { PostService } from './services/post.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,7 @@ import { AppRoutingModule } from './app.routing';
     FormsModule,ReactiveFormsModule,HttpModule,
     HttpClientModule,HttpClient
   ],
-  providers: [],
+  providers: [PostService,AuthenticationService,AuthGuard,UpdateService,ReportService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
