a8bdf4d89e16ead54cca10b28dccc4b3361d95ce
coresoft021 <38849384+coresoft021@users.noreply.github.com>
2018-08-15 13:09:13 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';  
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
@@ -47,7 +48,7 @@ import {
   MatSnackBarModule,
   MatTableModule,
   MatTooltipModule,
-    BrowserModule,
+    BrowserModule,BrowserAnimationsModule,
     AppRoutingModule,NguiAutoCompleteModule,
     FormsModule,ReactiveFormsModule,HttpModule,
     HttpClientModule
